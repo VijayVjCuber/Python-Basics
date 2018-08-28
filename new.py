ac1173d73098ba1a66860e491c5a158bada993f5
Vijay <41867265+VijayVjCuber@users.noreply.github.com>
2018-08-29 04:14:24 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,10 @@
+print ('Welcome to python programming')
+
+print ('Enter your name: ')
+Myname = input()
+print ('Hello Mr' + Myname + 'Welcome')
+print ('Enter your Age: ')
+Myage = input()
+print('your age will be ' + str(int(Myage)+2) + ' in 2020')
+print('The length of your name is:')
+print(len(Myname))
